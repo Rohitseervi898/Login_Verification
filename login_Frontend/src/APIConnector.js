1009e01e864469251a5d8ceb83c4b5e9fcd4ca7a
rohit8866seervi <rohitseervi8866@gmail.com>
2025-12-27 23:36:41 +0530

    final commit

--- login_Frontend/src/APIConnector.js
+++ login_Frontend/src/APIConnector.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
-const apiconnector= axios.create({baseURL:API_BASE_URL});
+const apiconnector= axios.create({
+    baseURL: API_BASE_URL ? `${API_BASE_URL}/api/user` : 'http://localhost:3000/api/user'
+});
 export default apiconnector;
