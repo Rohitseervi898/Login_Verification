b2fd171c6b4532bf4df4c7b0d16e3c8360d0f5ab
rohit8866seervi <rohitseervi8866@gmail.com>
2025-12-27 21:52:39 +0530

    small change

--- login_Frontend/src/APIConnector.js
+++ login_Frontend/src/APIConnector.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import dotenv from "dotenv";
+dotenv.config();
 
-const API_BASE_URL = 'http://localhost:3000/api/user';
+const API_BASE_URL = process.env.API;
 const apiconnector= axios.create({baseURL:API_BASE_URL});
 export default apiconnector;
